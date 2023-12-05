13b73618995991684bb67b48969cc6d8a5cfdd37
Khoi <dinhvietkhoi99@gmail.com>
2023-12-05 13:13:10 +0700

    udSv

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ export function verifyToken(req, res, next) {
     // console.log(req.headers['authorization'])
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+
     if (!token) {
             return res.status(401).json({ message: 'Không tìm thấy token' });
     }
@@ -11,8 +12,8 @@ export function verifyToken(req, res, next) {
     try {
         // Giải mã token và kiểm tra tính hợp lệ
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-
+        req.user = decoded.id;
+        console.log(req.user)
         next();
     } catch (err) {
         res.status(401).json({ message: 'Token không hợp lệ' });
