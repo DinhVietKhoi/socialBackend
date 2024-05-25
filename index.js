8f8d1672ada35ae7372054267f912eea5c9294b2
Khoi <dinhvietkhoi99@gmail.com>
2024-05-25 20:31:51 +0700

    fix-cors

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-import express from "express";
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import morgan from 'morgan'
+/** @format */
 
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 
 /* SET UP */
 const app = express();
 dotenv.config();
-
-//Bảo mật 
-app.use(helmet())
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+//Bảo mật
+app.use(helmet());
 
 //ghi lại thông tin tương tác
-app.use(morgan("common"))
-
-
-
-app.use(cors({
-    credentials:true
-}));
+app.use(morgan("common"));
 
 // Thiết lập giới hạn kích thước tối đa của yêu cầu
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.json())
+app.use(express.json());
 
 /* ROUTES */
-app.use("/v1/auth", authRoutes)
-app.use("/v1/users", userRoutes)
-app.use("/v1/posts", postRoutes)
+app.use("/v1/auth", authRoutes);
+app.use("/v1/users", userRoutes);
+app.use("/v1/posts", postRoutes);
 
 /* MONGOOSE SETUP*/
-const port = process.env.PORT || 6001
+const port = process.env.PORT || 6001;
 const mongoUrl = process.env.MONGO_URL;
 
 //Listen MongoDB to Server
-mongoose.connect(mongoUrl, {
+mongoose
+  .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    app.listen(port, ()=> console.log(`Máy chủ đang hoạt động tại cổng: ${port}`))
-}).catch((errors) => {
-    console.log(`${errors} lỗi kết nối máy chủ`)
-})
-
+  })
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Máy chủ đang hoạt động tại cổng: ${port}`)
+    );
+  })
+  .catch((errors) => {
+    console.log(`${errors} lỗi kết nối máy chủ`);
+  });
